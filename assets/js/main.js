da93a8a0a297c56d1c4c4cc5ad2058fb6eca06bb
John Zila <john@jzila.com>
2015-07-06 22:41:53 -0700

    Assigning to Object.prototype breaks many libraries, including jQuery and React. This fix is for those scenarios.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@
 	// Methods/polyfills.
 
 		// addEventsListener
-			Object.prototype.addEventsListener=function(t,e){var n,i=t.split(" ");for(n in i)this.addEventListener(i[n],e)}
+			var addEventsListener=function(o,t,e){var n,i=t.split(" ");for(n in i)o.addEventListener(i[n],e)}
 
 		// classList | (c) @remy | github.com/remy/polyfills | rem.mit-license.org
 			!function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
@@ -37,12 +37,12 @@
 			$navClose;
 
 		// Event: Prevent clicks/taps inside the nav from bubbling.
-			$nav.addEventsListener('click touchend', function(event) {
+			addEventsListener($nav, 'click touchend', function(event) {
 				event.stopPropagation();
 			});
 
 		// Event: Hide nav on body click/tap.
-			$body.addEventsListener('click touchend', function(event) {
+			addEventsListener($body, 'click touchend', function(event) {
 				$nav.classList.remove('visible');
 			});
 
